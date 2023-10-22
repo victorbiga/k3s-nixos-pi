e7faacdca230a366b3d331242849bd90ca8aba02
Victor Biga <victor.biga@gmail.com>
2023-10-22 13:30:32 +0200

    Update configuration.default.sdImage.nix

--- configuration.default.sdImage.nix
+++ configuration.default.sdImage.nix
@@ -29,8 +29,8 @@
 
   # systemPackages
   environment.systemPackages = with pkgs; [ 
-    vim curl wget nano bind kubectl helm iptables openvpn
-    python3 nodejs-12_x docker-compose ];
+    vim 
+  ];
 
   services.openssh = {
       enable = true;
@@ -49,11 +49,19 @@
   virtualisation.docker.enable = true;
 
   networking.firewall.enable = false;
+  networking.hostName = "master"; # Define your hostname.
+  networking.useDHCP = false;
+  networking.defaultGateway = "192.168.50.1";
+  networking.nameservers = [ "192.168.50.215" ];
+  networking.interfaces.eth0.useDHCP = false;
+  networking.interfaces.eth0.ipv4.addresses = [{
+    address = "192.168.50.177";
+    prefixLength = 24;
+  }];
 
-  # WiFi
+
   hardware = {
     enableRedistributableFirmware = true;
-    firmware = [ pkgs.wireless-regdb ];
   };
 
   # put your own configuration here, for example ssh keys:
@@ -66,17 +74,21 @@
     };
   };
   users.users = {
-    nixos = {
+    victor = {
       uid = 1000;
-      home = "/home/nixos";
-      name = "nixos";
+      home = "/home/victor";
+      name = "victor";
       group = "nixos";
       shell = pkgs.zsh;
-      extraGroups = [ "wheel" "docker" ];
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+	      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBq97CF4uE2oCUpEG4XNvwyGh8JUZnoNTHLTxnp6ZyDZyYs/sKqbmV9yCVXE4FGgWodVSZMDXN+Gi8x6wXt8D9E3AMCXecD1oGEwneYKaEb1FmOn9TySUpdo5nC4RATbEEbVWBtjPLuqm9LssHtzvw7i61AvrQpzgYjRPKrwakPHhtGFKZc9PjbGe0NmH0z4JDOEUvfzcK8uZkTPjvaSSF5zVtpa/1mMs2QApwZBmmSusJNmDgftZl5y/FInPUwnzs+5NPJCNwzywFKkrDYWuIHfygMDpuIcKPuyAOE3MOBmpw5V5JVmRQ/YaDUV1xhKrsn70WPsN3sSCNiaqe9UVdzv2uMZ34wweWJrWhBB0rZAXXhm7X1JsK+s5QdAYGAkiLAm5fJu5tv7vSWmdaQwhl/4FjebzE80+nLNn00zQmTSeMjEA29+4GN+QtunclshFWarc/Uwi12e1zGsHgv1T/UXI7aIXOgr5Q+LFVmyKIn7+9wh85MmZA3X25w19l+Zc= victor@Victors-MacBook-Pro.local"
+      ];
     };
   };
   users.extraUsers.root.openssh.authorizedKeys.keys = [
       # Your ssh key
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBq97CF4uE2oCUpEG4XNvwyGh8JUZnoNTHLTxnp6ZyDZyYs/sKqbmV9yCVXE4FGgWodVSZMDXN+Gi8x6wXt8D9E3AMCXecD1oGEwneYKaEb1FmOn9TySUpdo5nC4RATbEEbVWBtjPLuqm9LssHtzvw7i61AvrQpzgYjRPKrwakPHhtGFKZc9PjbGe0NmH0z4JDOEUvfzcK8uZkTPjvaSSF5zVtpa/1mMs2QApwZBmmSusJNmDgftZl5y/FInPUwnzs+5NPJCNwzywFKkrDYWuIHfygMDpuIcKPuyAOE3MOBmpw5V5JVmRQ/YaDUV1xhKrsn70WPsN3sSCNiaqe9UVdzv2uMZ34wweWJrWhBB0rZAXXhm7X1JsK+s5QdAYGAkiLAm5fJu5tv7vSWmdaQwhl/4FjebzE80+nLNn00zQmTSeMjEA29+4GN+QtunclshFWarc/Uwi12e1zGsHgv1T/UXI7aIXOgr5Q+LFVmyKIn7+9wh85MmZA3X25w19l+Zc= victor@Victors-MacBook-Pro.local"
   ];
+  security.sudo.wheelNeedsPassword = false;
 }
