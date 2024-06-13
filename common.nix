1a2f9b4c10820120c953a1683b9c2ded1927bd09
Victor Biga <victor.biga@gmail.com>
2024-06-13 17:33:54 +0200

    Update common.nix

--- common.nix
+++ common.nix
@@ -8,9 +8,7 @@
   users.users.victor = {
     hashedPassword = "$6$1x45qQgJwmCXXhbh$RQrnocus1l1NbKMbL17/9HyQ8RBarb.W33JBAQMjXZWBSs0YGsJBGljzHDJGMrs0KRp7gjiE8rgKbJfbYZlS50";
     isNormalUser = true;
-    extraGroups = [
-      "wheel"
-    ];
+    extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBq97CF4uE2oCUpEG4XNvwyGh8JUZnoNTHLTxnp6ZyDZyYs/sKqbmV9yCVXE4FGgWodVSZMDXN+Gi8x6wXt8D9E3AMCXecD1oGEwneYKaEb1FmOn9TySUpdo5nC4RATbEEbVWBtjPLuqm9LssHtzvw7i61AvrQpzgYjRPKrwakPHhtGFKZc9PjbGe0NmH0z4JDOEUvfzcK8uZkTPjvaSSF5zVtpa/1mMs2QApwZBmmSusJNmDgftZl5y/FInPUwnzs+5NPJCNwzywFKkrDYWuIHfygMDpuIcKPuyAOE3MOBmpw5V5JVmRQ/YaDUV1xhKrsn70WPsN3sSCNiaqe9UVdzv2uMZ34wweWJrWhBB0rZAXXhm7X1JsK+s5QdAYGAkiLAm5fJu5tv7vSWmdaQwhl/4FjebzE80+nLNn00zQmTSeMjEA29+4GN+QtunclshFWarc/Uwi12e1zGsHgv1T/UXI7aIXOgr5Q+LFVmyKIn7+9wh85MmZA3X25w19l+Zc= victor@Victors-MacBook-Pro.local"
     ];
@@ -31,15 +29,11 @@
     };
   };
 
-  security = {
-    sudo.wheelNeedsPassword = false;
-  };
+  security.sudo.wheelNeedsPassword = false;
 
   systemd.services.k3s.path = [ pkgs.ipset ];
 
-  system = {
-    stateVersion = "23.05";
-  };
+  system.stateVersion = "23.05";
 
   networking = {
     firewall = {
@@ -66,12 +60,11 @@
       "cgroup_memory=1"
       "cgroup_enable=memory"
     ];
-    kernelModules = [
-      "ceph"
-      "rbd"
-    ];
+    kernelModules = [ "ceph" "rbd" ];
   };
+
   environment.systemPackages = [ pkgs.git ];
+
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = ''
