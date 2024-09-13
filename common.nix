ea3e74b2cce387b67b35205cbfc40e6738aff432
Victor Biga <victor.biga@gmail.com>
2024-09-13 17:23:32 +0200

    feat: add package to get vcgencmd measure_temp

--- common.nix
+++ common.nix
@@ -68,7 +68,7 @@
     kernelModules = [ "rbd" ];
   };
 
-  environment.systemPackages = [ pkgs.git pkgs.nfs-utils ];
+  environment.systemPackages = [ pkgs.git pkgs.nfs-utils libraspberrypi ];
   environment.variables = {
     PATH = [
     "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/run/wrappers/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
