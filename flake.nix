36e92186d0911fa72cdf5ff8df834ad90eaebace
Victor Biga <victor.biga@gmail.com>
2024-11-08 18:04:36 +0100

    feat: add rpi5 configuration

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    raspberry-pi-nix.url = "github:nix-community/raspberry-pi-nix";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
+  outputs = { self, nixpkgs, nixos-hardware, raspberry-pi-nix }: {
     # Helper function to generate NixOS configurations for nodes
     mkNode = nodeConfig: nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";  # Architecture defined once
@@ -18,6 +19,16 @@
         nodeConfig  # Specific node configuration
       ];
     };
+    mkNode5 = nodeConfig: nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";  # Architecture defined once
+      modules = [
+        raspberry-pi-nix.nixosModules.raspberry-pi
+        "${nixpkgs}/nixos/modules/profiles/minimal.nix"
+        ./shared/common5.nix  # Common configurations for all nodes
+        ./shared/rpi5.nix
+        nodeConfig  # Specific node configuration
+      ];
+    };
 
     nixosConfigurations = {
       # Node configurations using the mkNode helper function
@@ -25,6 +36,7 @@
       kube-node-2 = self.mkNode ./nodes/kube-node-2.nix;
       kube-node-3 = self.mkNode ./nodes/kube-node-3.nix;
       kube-node-4 = self.mkNode ./nodes/kube-node-4.nix;
+      kube-node-6 = self.mkNode5 ./nodes/kube-node-6.nix;
     };
   };
 }
---
+++ shared/rpi5.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, ... }: {
+  # bcm2711 for rpi 3, 3+, 4, zero 2 w
+  # bcm2712 for rpi 5
+  # See the docs at:
+  # https://www.raspberrypi.com/documentation/computers/linux_kernel.html#native-build-configuration
+  raspberry-pi-nix.board = "bcm2712";
+
+  hardware = {
+    bluetooth.enable = true;
+    raspberry-pi = {
+      config = {
+        all = {
+          base-dt-params = {
+            # enable autoprobing of bluetooth driver
+            # https://github.com/raspberrypi/linux/blob/c8c99191e1419062ac8b668956d19e788865912a/arch/arm/boot/dts/overlays/README#L222-L224
+            krnbt = {
+              enable = true;
+              value = "on";
+            };
+          };
+        };
+      };
+    };
+  };
+
+  system.stateVersion = "24.05";
+}
